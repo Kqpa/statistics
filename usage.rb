cdbcedc51c48536e24f91ac18571fe87f93409c0
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-06-29 21:53:56 +0300

    changes to `statistics.rb`

--- usage.rb
+++ usage.rb
@@ -2,7 +2,7 @@ require "./utils/statistics"
 
 stat = Utils::Statistics.new
 
-arr = [3,2,1,4,4,5,5,5]
+arr = [1, 2]
 puts "Sum: #{stat.sum(arr)}"
 puts "Max: #{stat.max(arr)}"
 puts "Min: #{stat.min(arr)}"
@@ -10,3 +10,4 @@ puts "Range: #{stat.range(arr)}"
 puts "Median: #{stat.median(arr)}"
 puts "Mean: #{stat.mean(arr)}"
 puts "Mode: #{stat.mode(arr)}"
+puts stat.check_empty(arr)
--- utils/statistics.rb
+++ utils/statistics.rb
@@ -1,97 +1,113 @@
 module Utils
-  # @brief    Statistics class utility for the Ruby language
-  # @author   Kqpa
-  # @methods  sum([Array]),
-  #           max([Array]),
-  #           min([Array]),
-  #           range([Array]),
-  #           median([Array]),
-  #           mode([Array]),
-  #           mean([Array])
+	
+	# @brief    Statistics class utility for the Ruby language
+	# @author   Kqpa
+	# @methods  sum([Array]),
+	#           max([Array]),
+	#           min([Array]),
+	#           range([Array]),
+	#           median([Array]),
+	#           mode([Array]),
+	#           mean([Array]),
+	#			[Private] check_empty([Array])
+	
+	class Statistics
+		
+		# @brief Calculates the sum of all the numbers in the given array
+		# @param nums [Array] The array of numbers
+		# @return [Float | Int] The sum of all the numbers in the array
 
-  class Statistics
-    # @brief Calculates the sum of all the numbers in the given array
-    # @param nums [Array] The array of numbers
-    # @return [Float | Int] The sum of all the numbers in the array
+		def sum(nums)
+			check_empty(nums)
+			sum = 0
+			nums.each do |num|
+				sum += num
+			end
+			return sum
+		end
 
-    def sum(nums)
-      sum = 0
-      nums.each do |num|
-        sum += num
-      end
-      sum
-    end
+		# @brief Calculates the maximum value of the given array
+		# @param nums [Array] The array of numbers
+		# @return [Float | Int] The maximum value in the array
 
-    # @brief Calculates the maximum value of the given array
-    # @param nums [Array] The array of numbers
-    # @return [Float | Int] The maximum value in the array
+		def max(nums)
+			check_empty(nums)
+			max = nums[0]
+			nums.each do |num|
+				max = num if num > max
+			end
+			return max
+		end
 
-    def max(nums)
-      max = nums[0]
-      nums.each do |num|
-        max = num if num > max
-      end
-      max
-    end
+		# @brief Calculates the minimum value of the given array
+		# @param nums [Array] The array of numbers
+		# @return [Float | Int] The minimum value in the array
 
-    # @brief Calculates the minimum value of the given array
-    # @param nums [Array] The array of numbers
-    # @return [Float | Int] The minimum value in the array
+		def min(nums)
+			check_empty(nums)
+			min = nums[0]
+			nums.each do |num|
+				min = num if num < min
+			end
+			return min
+		end
 
-    def min(nums)
-      min = nums[0]
-      nums.each do |num|
-        min = num if num < min
-      end
-      min
-    end
+		# @brief Calculates the range of the given array
+		# @param nums [Array] The array of numbers
+		# @return [Float | Int] The range of the array
 
-    # @brief Calculates the range of the given array
-    # @param nums [Array] The array of numbers
-    # @return [Float | Int] The range of the array
+		def range(nums)
+			check_empty(nums)
+			return nums.max - nums.min
+		end
 
-    def range(nums)
-      # easier to read than max(nums) - min(nums) and maybe faster
-      nums.max - nums.min
-    end
+		# @brief Calculates the median of the given array
+		# @param nums [Array] The array of numbers
+		# @return [Float | Int] The median of the array
 
-    # @brief Calculates the median of the given array
-    # @param nums [Array] The array of numbers
-    # @return [Float | Int] The median of the array
+		def median(nums)
+			check_empty(nums)
+			nums.sort!
+			if nums.length.even?
+				return (nums[nums.length / 2] + nums[nums.length / 2 - 1]) / 2
+			else
+				return nums[nums.length / 2]
+			end
+		end
 
-    def median(nums)
-      nums.sort!
-      if nums.length.even?
-        (nums[nums.length / 2] + nums[nums.length / 2 - 1]) / 2
-      else
-        nums[nums.length / 2]
-      end
-    end
+		# @brief Calculates the mode of the given array
+		# @param nums [Array] The array of numbers
+		# @return [Float | Int] The mode of the array
 
-    # @brief Calculates the mode of the given array
-    # @param nums [Array] The array of numbers
-    # @return [Float | Int] The mode of the array
+		def mode(nums)
+			check_empty(nums)
+			mode = 0
+			count = 0
+			nums.each do |num|
+				if nums.count(num) > count
+					mode = num
+					count = nums.count(num)
+				end
+			end
+			return mode
+		end
 
-    def mode(nums)
-      mode = 0
-      count = 0
-      nums.each do |num|
-        if nums.count(num) > count
-          mode = num
-          count = nums.count(num)
-        end
-      end
-      mode
-    end
+		# @brief Calculates the mean (average) of the given array
+		# @param nums [Array] The array of numbers
+		# @return [Float | Int] The mode of the array
 
-    # @brief Calculates the mean (average) of the given array
-    # @param nums [Array] The array of numbers
-    # @return [Float | Int] The mode of the array
+		def mean(nums)
+			check_empty(nums)
+			return nums.sum / nums.length
+		end
+ 
+		# @brief Checks if the given array is empty
+		# @param nums [Array] The array of numbers
+		# @return [Raise] If the array is empty, raise an exception
+		private def check_empty(arr)
+			return raise ArgumentError, "Array is empty" if arr.empty?
+		end
 
-    def mean(nums)
-      return "can't divide by zero" if nums.empty?
-
-      sum(nums) / nums.length
-    end
-  end
+	end
+	
 end
